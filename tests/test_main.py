d77eae035351e8516c3e316c1c276d1e7348cd3b
913HudemaDana <hudema.dana@gmail.com>
2022-04-03 15:18:55 +0300

    the good one

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from battleship.main import main
-
-
-def test_main_general(cli_runner):
-    result = cli_runner.invoke(main)
-    assert 'Usage: main' in result.output
